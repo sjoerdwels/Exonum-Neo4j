0fa508b885454f5f4a2798788945d4c507f9b2dc
Indrek Klanberg <indrek@unionfintech.com>
2018-11-01 13:00:28 +0100

    initial .toml test using git to retrieve exonum library.

--- exonum-neo4j-service/src/main.rs
+++ exonum-neo4j-service/src/main.rs
@@ -1,3 +1,14 @@
+extern crate exonum;
+extern crate exonum_configuration;
+
+use exonum::helpers::{self, fabric::NodeBuilder};
+use exonum_configuration as configuration;
+
 fn main() {
-    println!("Hello, world!");
+    exonum::crypto::init();
+    helpers::init_logger().unwrap();
+
+    let node = NodeBuilder::new()//When I pass the parameters related to conf then does this handle it somehwo?
+        .with_service(Box::new(configuration::ServiceFactory))//So what does that do?
+    node.run();
 }
