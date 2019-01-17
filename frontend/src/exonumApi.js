7d455b10471f919d61ab6df4f961173c44398da1
Sjoerd Wels <sjoerd.wels@gmail.com>
2019-01-17 21:53:31 +0100

    Fixed jquery slowness, improved functionality. Added demo image.

--- frontend/src/exonumApi.js
+++ frontend/src/exonumApi.js
@@ -6,10 +6,9 @@ const TX_EXPLORER_URL = 'api/services/neo4j_blockchain/v1/transaction?hash_strin
 const PROTOCOL_VERSION = 0;
 const SERVICE_ID = 144;
 const TX_INSERT_QUERIES_ID = 0;
-const ATTEMPTS = 10;
+const ATTEMPTS = 0;
 const ATTEMPT_TIMEOUT = 500;
 
-// Messages
 const insertQueries = Exonum.newMessage({
     protocol_version: PROTOCOL_VERSION,
     service_id: SERVICE_ID,
@@ -20,7 +19,6 @@ const insertQueries = Exonum.newMessage({
     ]
 });
 
-// Keypairs
 const keyPair = {
     publicKey: process.env.EXONUM_PUBLIC_KEY,
     secretKey: process.env.EXONUM_PRIVATE_KEY
@@ -35,16 +33,21 @@ function sendTransaction(query) {
 
     const signature = insertQueries.sign(keyPair.secretKey, data);
 
-    return insertQueries.send(TX_URL  + "insert_transaction", TX_EXPLORER_URL, data, signature, ATTEMPTS, ATTEMPT_TIMEOUT)
-}
+    insertQueries.signature = signature;
 
+    let hash = insertQueries.hash(data);
+
+    return insertQueries.send(TX_URL  + "insert_transaction", TX_EXPLORER_URL, data, signature, ATTEMPTS, ATTEMPT_TIMEOUT).then(() => {
+        return { tx_hash : hash }
+    })
+}
 
 function getNodeHistory(uuid) {
     return axios.get(TX_URL  + `node_history?node_uuid=${uuid}`).then(response => response.data)
 }
 
 function getTransaction(hash) {
-    return axios.get(TX_URL  +`transactions?hash_string=${hash}`).then(response => response.data);
+    return axios.get(TX_URL  +`transaction?hash_string=${hash}`).then(response => response.data);
 }
 
 exports.sendTransaction = sendTransaction;
