e218d83f34b57dfaf701f06b53441b43501f47d6
Silver Vapper <silvervapper@live.com>
2019-01-22 19:58:52 +0100

    fix for integration testing run files

--- exonum-neo4j-service/runTestNode4.sh
+++ exonum-neo4j-service/runTestNode4.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-target/debug/exonum-neo4j run --node-config ../../shared-config/node_4_cfg.toml --db-path ../../shared-config/db4 --public-api-address 172.17.0.5:8200
+target/debug/exonum-neo4j run --node-config ../../shared-config/node_4_cfg.toml --db-path ../../shared-config/db4 --public-api-address 172.17.0.5:8200 &
 
