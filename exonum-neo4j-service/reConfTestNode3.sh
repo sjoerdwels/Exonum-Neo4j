1091acb235b0773dd633f442dd1f92146fc912bf
Indrek Klanberg <indrkl@gmail.com>
2019-01-22 14:16:07 +0100

    Added docker necessary files to development

---
+++ exonum-neo4j-service/reConfTestNode3.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+target/debug/exonum-neo4j generate-config ../../shared-config/common.toml  ../../shared-config/pub_3.toml ../../shared-config/sec_3.toml --peer-address 172.17.0.4:6331
+
