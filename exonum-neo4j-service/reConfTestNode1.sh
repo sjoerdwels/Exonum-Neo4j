ed5f789ee0fbb553ca597bf3c0dcd8351e94397f
Indrek Klanberg <indrkl@gmail.com>
2019-01-23 12:54:35 +0100

    Changed runtestNode files

--- exonum-neo4j-service/reConfTestNode1.sh
+++ exonum-neo4j-service/reConfTestNode1.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-target/debug/exonum-neo4j generate-config ../../shared-config/common.toml  ../../shared-config/pub_1.toml ../../shared-config/sec_1.toml --peer-address 172.17.0.2:6331 >> exo_neo.log
+target/debug/exonum-neo4j generate-config ../../shared-config/common.toml  ../../shared-config/pub_1.toml ../../shared-config/sec_1.toml --peer-address 172.17.0.2:6331
 
--- exonum-neo4j-service/runTestNode1.sh
+++ exonum-neo4j-service/runTestNode1.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-target/debug/exonum-neo4j run --node-config ../../shared-config/node_1_cfg.toml --db-path ../../shared-config/db1 --public-api-address 172.17.0.2:8200 &
+target/debug/exonum-neo4j run --node-config ../../shared-config/node_1_cfg.toml --db-path ../../shared-config/db1 --public-api-address 172.17.0.2:8200 >> exo_neo.log
 
