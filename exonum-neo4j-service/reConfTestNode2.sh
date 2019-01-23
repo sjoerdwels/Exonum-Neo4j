bfc98c753c75331232eb81527177ffe1a3a50973
Indrek Klanberg <indrkl@gmail.com>
2019-01-23 13:42:13 +0100

    Made changes to docker reconf and run scripts.

--- exonum-neo4j-service/reConfTestNode2.sh
+++ exonum-neo4j-service/reConfTestNode2.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-target/debug/exonum-neo4j generate-config ../../shared-config/common.toml  ../../shared-config/pub_2.toml ../../shared-config/sec_2.toml --peer-address 172.17.0.3:6331 >> exo_neo.log
+target/debug/exonum-neo4j generate-config ../../shared-config/common.toml  ../../shared-config/pub_2.toml ../../shared-config/sec_2.toml --peer-address 172.17.0.3:6331
 
