43e111518f7d238dc4324998c870111b93a01233
Indrek Klanberg <indrkl@gmail.com>
2019-01-23 12:40:18 +0100

    exonum service output will be written to file on runtime.

--- exonum-neo4j-service/reConfTestNode3.sh
+++ exonum-neo4j-service/reConfTestNode3.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-target/debug/exonum-neo4j generate-config ../../shared-config/common.toml  ../../shared-config/pub_3.toml ../../shared-config/sec_3.toml --peer-address 172.17.0.4:6331
+target/debug/exonum-neo4j generate-config ../../shared-config/common.toml  ../../shared-config/pub_3.toml ../../shared-config/sec_3.toml --peer-address 172.17.0.4:6331 >> exo_neo.log
 
