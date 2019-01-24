c35308ab59136e0b348ef46a180bfba7c2238e13
Indrek Klanberg <indrkl@gmail.com>
2019-01-24 13:28:03 +0100

    changed rights for conf file and other minor changes.

--- docker/linux_startup.sh
+++ docker/linux_startup.sh
@@ -13,34 +13,29 @@ frontend_port=3000
 for i in $(seq 1 $((node_count)))
 do
     echo docker run -t -d -i --name node$i -p 820$i:8200 -p 747$i:7474 -p 768$i:7687 -p 300$i:3005 -v $volume_name:/shared-config $image_name
-
     docker run -t -d -i --name node$i -p 820$i:8200 -p 747$i:7474 -p 768$i:7687 -p 300$i:3005 -v $volume_name:/shared-config $image_name
     docker exec -w /Exonum-Neo4j/ node$i git pull
     docker exec node$i neo4j start
 done
 
 #cleaning shared_config folder and making common conf
-docker exec -w /Exonum-Neo4j/backend/ node1 chmod +x genCommonConfigTesting.sh
 docker exec -w /Exonum-Neo4j/backend/ node1 ./genCommonConfigTesting.sh $node_count
 
 #Generating conf for each node
 for i in $(seq 1 $((node_count)))
 do
-    docker exec -w /Exonum-Neo4j/backend/ node$i chmod +x reConfTestNode.sh
     docker exec -w /Exonum-Neo4j/backend/ node$i ./reConfTestNode.sh $i
 done
 
 #Finalizing conf for each node
 for i in $(seq 1 $((node_count)))
 do
-    docker exec -w /Exonum-Neo4j/backend/ node$i chmod +x finalizeTesting.sh
     docker exec -w /Exonum-Neo4j/backend/ node$i ./finalizeTesting.sh $i $node_count
 done
 
 #Run all the nodes
 for i in $(seq 1 $((node_count)))
 do
-    docker exec -w /Exonum-Neo4j/backend/ node$i chmod +x runTestNode.sh
     docker exec -d -w /Exonum-Neo4j/backend/ node$i ./runTestNode.sh $i
 done
 
