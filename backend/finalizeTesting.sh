a2111f44e065d2fe7db93837d4b7479f71b4b584
Indrek Klanberg <indrkl@gmail.com>
2019-01-24 12:12:40 +0100

    minor fixes for conf and docker.

--- backend/finalizeTesting.sh
+++ backend/finalizeTesting.sh
@@ -5,7 +5,7 @@ num=$((node+1))
 pubNodes=""
 for i in $(seq 1 $((node_count)))
 do
-    pubNodes=$pubNodes ../../shared-config/pub_$i.toml
+    pubNodes=$pubNodes+"../../shared-config/pub_$i.toml"
 done
 echo target/debug/exonum-neo4j finalize --public-api-address 172.17.0.$num:8200 --private-api-address 172.17.0.$num:8091 ../../shared-config/sec_$node.toml ../../shared-config/node_$node_cfg.toml --public-configs $pubNodes
 target/debug/exonum-neo4j finalize --public-api-address 172.17.0.$num:8200 --private-api-address 172.17.0.$num:8091 ../../shared-config/sec_$node.toml ../../shared-config/node_$node_cfg.toml --public-configs $pubNodes
