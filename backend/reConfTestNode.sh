605c5cb6dab5f6f4bdcd3c3d8358ba9e80d56c91
Indrek Klanberg <indrkl@gmail.com>
2019-01-24 12:48:20 +0100

    ...

--- backend/reConfTestNode.sh
+++ backend/reConfTestNode.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 node=$1
 
-echo target/debug/exonum-neo4j generate-config ../../shared-config/common.toml  ../../shared-config/pub_node.toml ../../shared-config/sec_node.toml --peer-address 172.17.0.2:6331
-target/debug/exonum-neo4j generate-config ../../shared-config/common.toml  ../../shared-config/pub_node.toml ../../shared-config/sec_node.toml --peer-address 172.17.0.2:6331
+echo target/debug/exonum-neo4j generate-config ../../shared-config/common.toml  ../../shared-config/pub_$node.toml ../../shared-config/sec_$node.toml --peer-address 172.17.0.2:6331
+target/debug/exonum-neo4j generate-config ../../shared-config/common.toml  ../../shared-config/pub_$node.toml ../../shared-config/sec_$node.toml --peer-address 172.17.0.2:6331
