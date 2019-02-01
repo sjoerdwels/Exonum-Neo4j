bc7dcf587f070eb7c177c7f546b6d93db5ae288f
Indrek Klanberg <indrkl@gmail.com>
2019-02-01 16:53:52 +0100

    Now you can tell configs which to run, and unix startup set to release.

--- backend/genCommonConfigTesting.sh
+++ backend/genCommonConfigTesting.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-echo target/debug/exonum-neo4j generate-template ../../shared-config/common.toml --validators-count $1
+target = $2
+echo target/$target/exonum-neo4j generate-template ../../shared-config/common.toml --validators-count $1
 rm -r -f ../../shared-config/*
-target/debug/exonum-neo4j generate-template ../../shared-config/common.toml --validators-count $1
+target/$target/exonum-neo4j generate-template ../../shared-config/common.toml --validators-count $1
--- backend/runTestNode.sh
+++ backend/runTestNode.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 i=$1
+target=$2
 i_cfg=$i"_cfg"
-echo target/debug/exonum-neo4j run --node-config node_$i_cfg.toml --db-path db$i --public-api-address 172.17.0.$((i+1)):8200 >> exo_neo.log
-target/debug/exonum-neo4j run --node-config node_$i_cfg.toml --db-path db$i --public-api-address 172.17.0.$((i+1)):8200 >> exo_neo.log
+echo target/$target/exonum-neo4j run --node-config node_$i_cfg.toml --db-path db$i --public-api-address 172.17.0.$((i+1)):8200 >> exo_neo.log
+target/$target/exonum-neo4j run --node-config node_$i_cfg.toml --db-path db$i --public-api-address 172.17.0.$((i+1)):8200 >> exo_neo.log
