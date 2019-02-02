b5c961c709a111f84e48074b40c1775eed8f87ef
Indrek Klanberg <indrkl@gmail.com>
2019-02-02 18:24:38 +0100

    minor missspell in bash

--- backend/genCommonConfigTesting.sh
+++ backend/genCommonConfigTesting.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-target = $2
+target=$2
 echo target/$target/exonum-neo4j generate-template ../../shared-config/common.toml --validators-count $1
 rm -r -f ../../shared-config/*
 target/$target/exonum-neo4j generate-template ../../shared-config/common.toml --validators-count $1
